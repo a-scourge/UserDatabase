583460367df28dfaf1477d341a7a260f20d6ad7c
cessna-cowboy <gavinskigavarno@gmail.com>
2013-11-18 13:54:26 +0000

    start on update() override

---
+++ EngDatabase/EngDatabaseBase.pm
@@ -0,0 +1,11 @@
+package DBIx::Class::EngDatabaseBase;
+use base qw(DBIx::Class);
+
+sub update {
+    my $self = shift;
+    my $data = shift;
+    $self->set_inflated_columns($data);
+    my %changes = $self->get_dirty_columns;
+    return $self->next::method;
+}
+
